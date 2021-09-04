f7e29717533b1a53854d2f5b80c36740462414a0
adambergman123 <73640809+adambergman123@users.noreply.github.com>
2021-09-04 23:47:51 +0200

    Revert "Revert "Revert "cool shit"""
    This reverts commit 7cfe942c687a9f24fb4b8399bc13c1da107c8571.

--- src/Components/Home/Boisen.js
+++ src/Components/Home/Boisen.js
@@ -4,30 +4,12 @@ import './Boisen.css'
 
 const Wrapper = styled.div`
   width: 100vw;
-  height: 800px;
+
   color: white;
-  background: #130d0a;
-`
-
-const Axel = styled.div`
-  width: 100%;
-  height: 800px;
-  display: flex;
-`
-
-const AxelImage = styled.div`
-  width: 50%;
 `
 
 const Boisen = () => {
-  return (
-    <Wrapper>
-      <Axel>
-        <AxelImage className='axelImage' />
-        <div className='glass'></div>
-      </Axel>
-    </Wrapper>
-  )
+  return <Wrapper></Wrapper>
 }
 
 export default Boisen
