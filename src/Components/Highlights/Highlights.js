24921f03ee52826eefd9d378100a83d539ba8832
adambergman123 <73640809+adambergman123@users.noreply.github.com>
2021-11-18 10:41:11 +0100

    fffff
    ff

--- src/Components/Highlights/Highlights.js
+++ src/Components/Highlights/Highlights.js
@@ -4,7 +4,6 @@ import { motion } from 'framer-motion'
 
 const MainWrapper = styled(motion.div)`
   width: 100vw;
-  background: lightblue;
 
   display: flex;
   flex-direction: column;
@@ -13,21 +12,54 @@ const MainWrapper = styled(motion.div)`
 
 const ContentWrapper = styled.div`
   width: 88vw;
-  max-width: 1500px;
+  max-width: 1400px;
 `
 
-const Divider = styled.div`
-  height: 10px;
+const Divider = styled.hr`
+  border-top: 8px solid rgb(33, 36, 48);
+  border-radius: 5px;
   width: 100%;
-  background: white;
-  padding-top: 20px;
-  padding-bottom: 20px;
+  margin-top: 10px;
+  margin-bottom: 20px;
 `
 
 const HighlightsTitle = styled.div`
-  height: 214px;
+  background-image: linear-gradient(
+    270deg,
+    rgb(229, 93, 135) -0.52%,
+    rgb(95, 195, 228) 100%
+  );
+`
+const HighlightsTitleGrid = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 50% 50%;
 
-  background: pink;
+  @media screen and (max-width: 1050px) {
+    grid-template-columns: 100%;
+  }
+`
+const HighlightsText = styled.div`
+  margin-left: 20px;
+  padding: 35px;
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+`
+
+const TopText = styled.div`
+  font-size: 32px;
+  font-family: Wotfard-Semibold;
+  color: rgb(231, 231, 233);
+`
+
+const BottomText = styled.div`
+  padding-top: 10px;
+  padding-bottom: 5px;
+  font-size: 18px;
+  font-family: Wotfard;
+  color: rgb(231, 231, 233);
 `
 
 const Highlights = () => {
@@ -39,8 +71,18 @@ const Highlights = () => {
       transition={{ duration: 0.3, ease: 'easeInOut' }}
     >
       <ContentWrapper>
-        <Divider />
-        <HighlightsTitle>Highlights</HighlightsTitle>
+        <Divider className='rounded' />
+        <HighlightsTitle className='rounded-lg shadow-lg'>
+          <HighlightsTitleGrid>
+            <HighlightsText>
+              <TopText>Welcome to CodeSnap!</TopText>
+              <BottomText>
+                Hey! My name's Sam, and I'm making video courses which teach you
+                to make cool stuff for the web, mobile, and beyond.
+              </BottomText>
+            </HighlightsText>
+          </HighlightsTitleGrid>
+        </HighlightsTitle>
       </ContentWrapper>
     </MainWrapper>
   )
